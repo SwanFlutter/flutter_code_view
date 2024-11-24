fa95959b7a701f36c5acee3b728ef0a218a651e5
SwanFlutter1993 <swan.dev1993@gmail.com>
2024-11-23 23:28:32 -0800

    fix publish error

---
+++ lib/src/themes/night_owl.dart
@@ -0,0 +1,52 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:flutter/painting.dart';
+
+const nightOwlTheme = {
+  'root':
+      TextStyle(backgroundColor: Color(0xFF011627), color: Color(0xFFD6DEEB)),
+  'keyword': TextStyle(color: Color(0xFFC792EA), fontStyle: FontStyle.italic),
+  'built_in': TextStyle(color: Color(0xFFADDB67), fontStyle: FontStyle.italic),
+  'type': TextStyle(color: Color(0xFF82AAFF)),
+  'literal': TextStyle(color: Color(0xFFFF5874)),
+  'number': TextStyle(color: Color(0xFFF78C6C)),
+  'regexp': TextStyle(color: Color(0xFF5CA7E4)),
+  'string': TextStyle(color: Color(0xFFECC48D)),
+  'subst': TextStyle(color: Color(0xFFD3423E)),
+  'symbol': TextStyle(color: Color(0xFF82AAFF)),
+  'class': TextStyle(color: Color(0xFFFFCB8B)),
+  'function': TextStyle(color: Color(0xFF82AAFF)),
+  'title': TextStyle(color: Color(0xFFDCDCAA), fontStyle: FontStyle.italic),
+  'params': TextStyle(color: Color(0xFF7FDBCA)),
+  'comment': TextStyle(color: Color(0xFF637777), fontStyle: FontStyle.italic),
+  'doctag': TextStyle(color: Color(0xFF7FDBCA)),
+  'meta': TextStyle(color: Color(0xFF82AAFF)),
+  'meta-keyword': TextStyle(color: Color(0xFF82AAFF)),
+  'meta-string': TextStyle(color: Color(0xFFECC48D)),
+  'section': TextStyle(color: Color(0xFF82B1FF)),
+  'tag': TextStyle(color: Color(0xFF7FDBCA)),
+  'name': TextStyle(color: Color(0xFF7FDBCA)),
+  'builtin-name': TextStyle(color: Color(0xFF7FDBCA)),
+  'attr': TextStyle(color: Color(0xFF7FDBCA)),
+  'attribute': TextStyle(color: Color(0xFF80CBC4)),
+  'variable': TextStyle(color: Color(0xFFADDB67)),
+  'bullet': TextStyle(color: Color(0xFFD9F5DD)),
+  'code': TextStyle(color: Color(0xFF80CBC4)),
+  'emphasis': TextStyle(color: Color(0xFFC792EA), fontStyle: FontStyle.italic),
+  'strong': TextStyle(color: Color(0xFFADDB67), fontWeight: FontWeight.bold),
+  'formula': TextStyle(color: Color(0xFFC792EA)),
+  'link': TextStyle(color: Color(0xFFFF869A)),
+  'quote': TextStyle(color: Color(0xFF697098), fontStyle: FontStyle.italic),
+  'selector-tag': TextStyle(color: Color(0xFFFF6363)),
+  'selector-id': TextStyle(color: Color(0xFFFAD430)),
+  'selector-class':
+      TextStyle(color: Color(0xFFADDB67), fontStyle: FontStyle.italic),
+  'selector-attr':
+      TextStyle(color: Color(0xFFC792EA), fontStyle: FontStyle.italic),
+  'selector-pseudo':
+      TextStyle(color: Color(0xFFC792EA), fontStyle: FontStyle.italic),
+  'template-tag': TextStyle(color: Color(0xFFC792EA)),
+  'template-variable': TextStyle(color: Color(0xFFADDB67)),
+  'addition': TextStyle(color: Color(0xFFADDB67), fontStyle: FontStyle.italic),
+  'deletion': TextStyle(color: Color(0xFFEF5350), fontStyle: FontStyle.italic),
+};
